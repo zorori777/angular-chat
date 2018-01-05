7dd7931de2fe5f0486c8a50afd18f1d4a994cbe4
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-01-05 22:35:39 +0900

    [add] added assosiate with db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Comment } from './class/comment';
 import { User } from './class/user';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 const CURRENT_USER: User = new User(1, "わとそん");
 const ANOTHER_USER: User = new User(2, "たくみん")
@@ -19,9 +21,15 @@ const COMMENTS: Comment[] = [
 })
 
 export class AppComponent {
+
+  item: Observable<any>;
   comments = COMMENTS;
   currentUser = CURRENT_USER;
   content = '';
+
+  constructor(private db: AngularFireDatabase) {
+    this.item = db.object('/item').valueChanges();
+  }
 
   addComment(comment: string): void {
     if(comment) {
